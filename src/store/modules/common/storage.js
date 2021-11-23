ff8505ac6c2eb0e51ed5fd2782d5e0deda97c187
GeonBbang <gun4930@naver.com>
2021-11-23 18:29:49 +0900

    jsdoc수정 및 리팩토링

--- src/store/modules/common/storage.js
+++ src/store/modules/common/storage.js
@@ -1,39 +1,59 @@
 import axios from "axios";
 
 export default class DataManager {
+    /**
+     * 가장 최신 Log를 가져온다.
+     *
+     * @returns {(Promise<{loginId:string, isRemembered:boolean, isError:boolean}>)} Log정보, 에러유무가 들어있는 객체
+     */
     async getLoginLog() {
         try {
             const logResponse = await axios.post("http://localhost:4040/login-log/get");
-            const { 
-                data: { loginId, isRemembered },
+            const {
+                data: { loginId = "", isRemembered = false },
                 status
             } = logResponse;
-            
+
             if(status !== 200) {
                 throw new Error("[DB] Login Log Get 에러");
             }
             return { loginId, isRemembered, isError: false };
         } catch(e) {
             console.log(e);
-            return { isError: true };
+            return { loginId: "", isRemembered: false, isError: true };
         }
     }
+
+    /**
+     * 로그를 저장하고 에러 유무를 반환한다.
+     *
+     * @param {string} loginId 로그인 아이디
+     * @param {boolean} isRemembered 저장했는지 확인하는 값
+     * @returns {Promise<boolean>} 에러 유무
+     */
     async saveLoginLog(loginId, isRemembered) {
         try {
             const logResponse = await axios.post("http://localhost:4040/login-log/save", { loginId, isRemembered });
             if(logResponse.status !== 201) {
                 throw new Error("[DB] Login Log Update 에러");
             }
-            return { isError: false };
+            return false;
         } catch(e) {
             console.log(e);
-            return { isError: true };
+            return true;
         }
     }
+
+    /**
+     * loginId에 해당하는 유저 정보를 가져와서 반환한다.
+     *
+     * @param {string} loginId
+     * @returns {Promise<{userInfo: object, isError:boolean}>} 유저 정보와 에러 유무 반환
+     */
     async getUserInfo(loginId) {
         try {
             const userResponse = await axios.post("http://localhost:4040/user/get", { loginId });
-            const { 
+            const {
                 data: { userInfo } ,
                 status
             } = userResponse;
@@ -43,19 +63,26 @@ export default class DataManager {
             return { userInfo, isError: false };
         } catch(e) {
             console.log(e);
-            return { isError: true };
+            return { userInfo: {}, isError: true };
         }
     }
+
+    /**
+     * 유저정보를 저장하고 에러 유무를 반환한다.
+     *
+     * @param {object} userInfo 유저정보가 들어있는 객체
+     * @returns {Promise<boolean>} 에러 유무
+     */
     async saveUserInfo(userInfo) {
         try {
             const userResponse = await axios.post("http://localhost:4040/user/update", userInfo);
             if(userResponse.status !== 201) {
                 throw new Error("[DB] User Update 에러");
             }
-            return { isError: false };
+            return false;
         } catch(e) {
             console.log(e);
-            return { isError: true };
+            return true;
         }
     }
 }
--- src/store/modules/login.js
+++ src/store/modules/login.js
@@ -3,14 +3,14 @@ const dataManager = new DataManager();
 
 const actions = {
     /**
-     * LocalStorage에서 Loginid를 가져와서 만약 LoginId가 있다면 해당 LoginId로 유저정보를 DB에서
+     * Log DB에서 최근 Loginid를 가져와서 만약 기억된 상태라면 해당 LoginId로 유저정보를 User DB에서
      * 꺼내온 뒤 Login Page의 Input을 채워준다.
-     * 
-     * @param {import("vuex").Commit} commit Vuex store의 actions인자 안에 있는 commit
+     *
+     * @param {import("vuex").Store} commit Vuex store의 actions인자 안에 있는 commit
      */
     async loadAndInitUserInfo({ commit }) {
         try {
-            const { isError, loginId, isRemembered } = await dataManager.getLoginLog();
+            const { loginId, isRemembered, isError } = await dataManager.getLoginLog();
             if(isError) {
                 throw new Error("[DB] 에러");
             }
@@ -28,22 +28,22 @@ const actions = {
         }
     },
     /**
-     * 만약 정보를 기억하는 boolean이 true라면 loginId를 LocalStorage에 저장하고 해당 정보들을 db에 저장하거나 업데이트한다.
+     * 만약 정보를 기억하는 boolean이 true라면 loginId를 Log DB에 저장하고 해당 정보들을 User DB에 저장하거나 업데이트한다.
      * 만약 false라면 유저 정보에 대한 값들을 공백으로 초기화시킨다.
-     * 
-     * @param {} null 쓰이지 않는 인자값
+     *
+     * @param {null} null 쓰이지 않는 인자값
      * @param {object} userInfo 로그인 아이디, 닉네임, 아바타, 정보를 기억할지의 Boolean값을 담은 Object
      */
-    async checkAndSaveUserInfo({}, userInfo) {
+    async checkAndSaveUserInfo(_, userInfo) {
         const { loginId, chkRemember } = userInfo;
         try {
             if(chkRemember) {
-                const { isError } = await dataManager.saveUserInfo(userInfo);
+                const isError = await dataManager.saveUserInfo(userInfo);
                 if(isError) {
                     throw new Error("[DB] 에러");
                 }
             }
-            const { isError } = await dataManager.saveLoginLog(loginId, chkRemember);
+            const isError = await dataManager.saveLoginLog(loginId, chkRemember);
             if(isError) {
                 throw new Error("[DB] 에러");
             }
@@ -54,6 +54,6 @@ const actions = {
 }
 
 export default {
-    namespaced: true,
-    actions
+  namespaced: true,
+  actions
 }
